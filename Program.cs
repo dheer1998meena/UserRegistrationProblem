d28763180adfefada02b643286ebb09e8801c0f8
dheer meena <dheer1998meena@gmail.com>
2020-10-07 17:56:28 +0530

    [Dheer] Add . UserRegistrationProblem

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserRegistrationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateDetails details = new ValidateDetails();
+            Console.WriteLine("Enter the user first name :");
+            string firstName = Console.ReadLine();
+            while (!details.FirstNameValidation(firstName))
+            {
+                Console.WriteLine("Enter a valid first name : ");
+                firstName = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ ValidateDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProgram
+{
+    class ValidateDetails
+    {
+        public Boolean FirstNameValidation(string fname)
+        {
+            Regex regex = new Regex("^[A-Z][a-z]{2,}$");
+            if (regex.IsMatch(fname))
+                return true;
+            else
+                return false;
+        }
+    }
+}
